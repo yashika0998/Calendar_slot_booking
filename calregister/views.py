fc72b0f8ab9268676da976aee4cee3bd77f5944d
yashika0998 <yashika@gmail.com>
2020-06-21 01:58:14 +0530

    commiting the files

---
+++ calregister/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    return render(request, 'calregister/home.html', {'title': 'Home'})
+
+
+def about(request):
+    return render(request, 'calregister/about.html', {'title': 'About'})
---
+++ users_registration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UsersRegistrationConfig(AppConfig):
+    name = 'users_registration'
